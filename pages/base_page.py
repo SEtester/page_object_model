6c5cf2835bbc0b8fbaaa1cbd43b6c2bc99cde4e2
SEtester <dengjiajie0109@gmail.com>
2018-09-20 13:24:51 +0800

    命令行提交

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,7 +3,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from pprint import pprint
 
 class BasePage():
 

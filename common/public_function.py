8620f00668e1fd105c51088181e4098957699f3f
SEtester <dengjiajie0109@gmail.com>
2018-10-22 13:45:19 +0800

    封装select(一) —— 选择单一下拉框

--- common/public_function.py
+++ common/public_function.py
@@ -1,24 +1,24 @@
 # encoding:utf8
 
-from selenium.common.exceptions import TimeoutException
-import time
-
-
-def verify_msg_time(method, sleep_frequency, timeout, message=''):
-    '''用来判断验证码间隔时间'''
-    # count_time = 0
-    now_time = time.time()
-    end_time = time.time() + timeout
-    while True:
-        if method == False:
-            # count_time = count_time + 1
-            # print(count_time)
-            time.sleep(sleep_frequency)
-            now_time = now_time + sleep_frequency
-            if now_time < end_time:
-                continue
-            else:
-                raise TimeoutException(message)
-        else:
-            print(method)
-            break
+# from selenium.common.exceptions import TimeoutException
+# import time
+#
+#
+# def verify_msg_time(method, sleep_frequency, timeout, message=''):
+#     '''用来判断验证码间隔时间'''
+#     # count_time = 0
+#     now_time = time.time()
+#     end_time = time.time() + timeout
+#     while True:
+#         if method == False:
+#             # count_time = count_time + 1
+#             # print(count_time)
+#             time.sleep(sleep_frequency)
+#             now_time = now_time + sleep_frequency
+#             if now_time < end_time:
+#                 continue
+#             else:
+#                 raise TimeoutException(message)
+#         else:
+#             print(method)
+#             break
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,18 @@
 # encoding:utf8
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait,Select
 from selenium.webdriver.support import expected_conditions as EC
 from pprint import pprint
-import time
+from selenium.webdriver.support.select import Select
+
 
 class BasePage():
 
     def __init__(self, driver, path=None):
         # self.url = 'https://github.com'
-        self.url = 'https://mail.163.com/'
+        self.url = 'https://www.baidu.com'
+        # self.driver = webdriver.Chrome()
         self.driver = driver
         self.driver.maximize_window()
         self.load_page(path)
@@ -31,9 +33,9 @@ class BasePage():
     #     self.wait_element_visibility_of_element_located(locator)
     #     return self.driver.find_element(*locator)
 
-    def by_xpath(self, xpath,text=None):
+    def by_xpath(self, xpath, text=None):
         locator = (By.XPATH, xpath)
-        self.wait(locator,text)
+        self.wait(locator, text)
         return self.driver.find_element(*locator)
 
     def wait_element_visibility_of_element_located(self, locator):
@@ -48,33 +50,43 @@ class BasePage():
         WebDriverWait(self.driver, self.timeout, self.poll_frequency).until(
             EC.text_to_be_present_in_element_value(locator, text))
 
-    def by_css(self,css,text=None):
+    def by_css(self, css, text=None):
         locator = (By.CSS_SELECTOR, css)
         # self.wait_element_visibility_of_element_located(locator)
-        self.wait(locator,text)
+        self.wait(locator, text)
         return self.driver.find_element(*locator)
 
-    def wait(self,locator,text=None):
+    def wait(self, locator, text=None):
         if text == None:
             self.wait_element_visibility_of_element_located(locator)
         else:
-            self.wait_text_to_be_present_in_element(locator,text)
+            self.wait_text_to_be_present_in_element(locator, text)
 
-
-
-    def switch_to_frame(self,choose_iframe=None,css=None,xpath=None):
+    def switch_to_frame(self, choose_iframe=None, css=None, xpath=None):
         if choose_iframe == 'default':
             return self.driver.switch_to.default_content()
         elif choose_iframe == 'parent':
             return self.driver.switch_to.parent_frame()
         elif css != None and xpath == None:
             locator = (By.CSS_SELECTOR, css)
-        elif xpath != None and css == None :
+        elif xpath != None and css == None:
             locator = (By.XPATH, xpath)
         else:
             raise ValueError('参数错误，请传css定位或者xpath,需要指定用什么方式传')
         WebDriverWait(self.driver, self.timeout, self.poll_frequency).until(
             EC.frame_to_be_available_and_switch_to_it(locator))
+
+    def select_by(self,element,by,value):
+        select_element = element
+        if by == 'index':
+            s = Select(select_element).select_by_index(value)
+        elif by == 'value':
+            s = Select(select_element).select_by_value(value)
+        elif by == 'text':
+            s = Select(select_element).select_by_visible_text(value)
+        else:
+            raise ValueError()
+        return s
 
 
 
@@ -86,9 +98,17 @@ if __name__ == '__main__':
     # logging.basicConfig(level=logging.DEBUG)
     dr = webdriver.Chrome()
     print(dr.name)
-    # b = BasePage(dr, '/')
-    #
-    #
+    b = BasePage(dr, '/')
+    b.by_css('#u1 a:nth-child(8)').click()
+    b.by_css('.setpref').click()
+    s = b.by_css('#nr')
+    b.select_by(s,'text','每页显示50条')
+
+
+
+
+
+
     # # b.switch_to_frame(css='#x-URS-iframe')
     # b.switch_to_frame(xpath='//*[@id="x-URS-iframe"]')
     # b.by_css('.j-inputtext.dlemail').send_keys('123456')

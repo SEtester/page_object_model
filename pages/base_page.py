d5b533c5629806f106b10992d8ea31e6db535197
SEtester <dengjiajie0109@gmail.com>
2018-10-28 15:23:43 +0800

    删除爬虫脚本

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,8 +3,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait,Select
 from selenium.webdriver.support import expected_conditions as EC
-from pprint import pprint
-from selenium.webdriver.support.select import Select
+# from pprint import pprint
+# from selenium.webdriver.support.select import Select
 
 
 class BasePage():
